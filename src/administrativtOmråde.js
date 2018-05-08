263eef379023079aec6e6b3f692e97d40d2e504c
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2018-05-08 14:52:57 +0200

    feat(typer): Koder for administrative områder

---
+++ src/administrativtOmråde.js
@@ -0,0 +1,10 @@
+const Kodesystem = require("./kodesystem");
+
+// Natur i Norge (NiN) kodesystem
+class Natursystem extends Kodesystem {
+  constructor() {
+    super("AO");
+  }
+}
+
+module.exports = Natursystem;

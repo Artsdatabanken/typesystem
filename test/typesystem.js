b946be160ab0445c4936cf9bad4170225457df07
amned <amund.nedland@norconsult.com>
2018-06-08 10:21:16 +0200

    Egen funksjon som splitter med målestokk

--- test/typesystem.js
+++ test/typesystem.js
@@ -5,6 +5,10 @@ describe("typesystem", function() {
   it("splitt T44-E-1", function() {
     assert.equal(typesystem.splittKode("NA_T44-E-1").join("/"), "NA/T/44/E/1");
   });
+    it("splitt T44-E-1", function() {
+        assert.equal(typesystem.splittKodeMalestokk("NA_T44-E-1").join("/"), "NA/T/44/E-1");
+    });
+
   it("Davvisámegiella med gyldige tegn", function() {
     assert.equal(
       typesystem.medGyldigeTegn("Davvisámegiella"),

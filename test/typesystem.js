b644bd19fd4a4f60abc5fcf5690ee6e54257644d
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-01-27 11:14:24 +0100

    feat: Flytt Natur i Norge under egen toppnode (#43)
    BREAKING CHANGE: Nytt hierarki for landskap og natursystem

--- test/typesystem.js
+++ test/typesystem.js
@@ -1,48 +1,54 @@
-var assert = require("assert")
-var typesystem = require("../src/index")
+var assert = require("assert");
+var typesystem = require("../src/index");
 
 describe("typesystem", function() {
   it("splitt T44-E-1", function() {
-    assert.equal(typesystem.splittKode("NA-T44-E-1").join("/"), "NA/T/44/E-1")
-  })
+    assert.equal(typesystem.splittKode("NA-T44-E-1").join("/"), "NA/T/44/E-1");
+  });
   it("splitt BS-1_AG_A_0", function() {
-    assert.equal(typesystem.splittKode("BS-1_AG_A_0").join("/"), "BS/1/AG/A/0")
-  })
+    assert.equal(typesystem.splittKode("BS-1_AG_A_0").join("/"), "BS/1/AG/A/0");
+  });
   it("forelder T44", function() {
-    assert.equal(typesystem.forelder("NA-T44"), "NA-T")
-  })
+    assert.equal(typesystem.forelder("NA-T44"), "NA-T");
+  });
   it("forelder T44-E1-C2", function() {
-    assert.equal(typesystem.forelder("NA-T44-E-2"), "NA-T44")
-  })
+    assert.equal(typesystem.forelder("NA-T44-E-2"), "NA-T44");
+  });
   it("forelder NA-LKM-S3-E-0", function() {
-    assert.equal(typesystem.forelder("NA-LKM-S3-E-0"), "NA-LKM-S3-E")
-  })
+    assert.equal(typesystem.forelder("NA-LKM-S3-E-0"), "NA-LKM-S3-E");
+  });
 
   it("Davvisámegiella med gyldige tegn", function() {
-    assert.equal(typesystem.medGyldigeTegn("Davvisámegiella"), "Davvismegiella")
-  })
+    assert.equal(
+      typesystem.medGyldigeTegn("Davvisámegiella"),
+      "Davvismegiella"
+    );
+  });
   it("Kapitaliserer canis lupus", function() {
-    assert.equal(typesystem.capitalizeTittel("cAnis LuPus"), "Canis lupus")
-  })
+    assert.equal(typesystem.capitalizeTittel("cAnis LuPus"), "Canis lupus");
+  });
   it("Nivå for åker", function() {
     assert.equal(
-      typesystem.hentNivaa("NA-T44").join(","),
+      typesystem.hentNivaa("NN-NA-T44").join(","),
       "hovedtype,hovedtypegruppe,naturmangfold"
-    )
-  })
+    );
+  });
+  it("Nivå for Børgefjell", function() {
+    assert.equal(typesystem.hentNivaa("VV-42").join(","), "naturvernområde");
+  });
   it("NA foreldre", function() {
-    assert.equal(typesystem.forfedre("NA").join(","), "NA")
-  })
+    assert.equal(typesystem.forfedre("NA").join(","), "NA");
+  });
   it("T44-B-1 foreldre", function() {
     assert.equal(
       typesystem.forfedre("NA-T44-B-1").join(","),
       "NA,NA-T,NA-T44,NA-T44-B,NA-T44-B-1"
-    )
-  })
+    );
+  });
   it("T44-B-1 foreldre", function() {
     assert.equal(
       typesystem.forfedre("NA-T44-B-1").join(","),
       "NA,NA-T,NA-T44,NA-T44-B,NA-T44-B-1"
-    )
-  })
-})
+    );
+  });
+});

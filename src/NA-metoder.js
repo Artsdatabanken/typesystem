edbd76c9bcde08f87274ac1d45476f27bf0c44bb
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-01-30 12:46:07 +0100

    fix(natursystem): Kartleggingsenheter under hovedtype, ikke på rot (#44)
    BREAKING CHANGE: Omstrukturert tre

--- src/NA-metoder.js
+++ src/NA-metoder.js
@@ -1,24 +1,24 @@
-const NA = require("./NA")
-const felles = require("./felles")
+const NA = require("./NA");
+const felles = require("./felles");
 
 // Natur i Norge (NiN) kodesystem
 const Natursystem = {
   // Slår opp hovedtypen til en grunntype eller kartleggingsenhet
   slåOppHovedtype: function(subkode) {
-    const ofs = subkode.indexOf("-", 3)
-    if (ofs < 0) return subkode
-    return subkode.substring(0, ofs)
+    const ofs = subkode.indexOf("-", 6);
+    if (ofs < 0) return subkode;
+    return subkode.substring(0, ofs);
   },
 
   // Sjekker om koden er en NiN grunntype
   erGrunntype: function(kode) {
-    return !!kode.match(/NA-[A-Z][0-9]+-[0-9]+/gi)
+    return !!kode.match(/NA-[A-Z][0-9]+-[0-9]+/gi);
   },
 
   // Sjekker om koden er på kartleggingsnivå over grunntype (1:500)
   erHøyereKartleggingsnivå: function(kode) {
-    return !!kode.match(/-[BCDE]-/gi)
+    return !!kode.match(/-[BCDE]-/gi);
   }
-}
+};
 
-module.exports = { ...felles, ...Natursystem, ...NA }
+module.exports = { ...felles, ...Natursystem, ...NA };
--- test/natursystem.js
+++ test/natursystem.js
@@ -1,23 +1,23 @@
-var assert = require("assert")
-var natursystem = require("../src/NA-metoder")
+var assert = require("assert");
+var natursystem = require("../src/NA-metoder");
 
 describe("Natursystem", function() {
   it("hovedtype", function() {
-    assert.equal(natursystem.slåOppHovedtype("NA-T44-E-1"), "NA-T44")
-  })
+    assert.equal(natursystem.slåOppHovedtype("NN-NA-T44-E-1"), "NN-NA-T44");
+  });
   it("T44-E-1 er ikke grunntype", function() {
-    assert.equal(natursystem.erGrunntype("NA-T44-E-1"), false)
-  })
+    assert.equal(natursystem.erGrunntype("NA-T44-E-1"), false);
+  });
   it("T44-1 er grunntype", function() {
-    assert.equal(natursystem.erGrunntype("NA-T44-1"), true)
-  })
+    assert.equal(natursystem.erGrunntype("NA-T44-1"), true);
+  });
   it("T44-1 erHøyereKartleggingsnivå: nei", function() {
-    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-1"), false)
-  })
+    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-1"), false);
+  });
   it("T44-A-1 erHøyereKartleggingsnivå: nei", function() {
-    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-A-1"), false)
-  })
+    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-A-1"), false);
+  });
   it("T44-B-1 erHøyereKartleggingsnivå: ja", function() {
-    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-B-1"), true)
-  })
-})
+    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-B-1"), true);
+  });
+});

e8bb46e63cb2d3854b8f9d968d38a2c551331fa2
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-03-23 15:09:11 +0100

    feat(Hierarki): Nytt nivå typeinndeling over typene i natursystem og landskap (#61)
    * feat(Hierarki): Nytt nivå typeinndeling over typene i natursystem og landskap

--- test/natursystem.js
+++ test/natursystem.js
@@ -6,18 +6,24 @@ describe("Natursystem", function() {
     assert.equal(natursystem.slåOppHovedtype("NN-NA-T44-E-1"), "NN-NA-T44");
   });
   it("T44-E-1 er ikke grunntype", function() {
-    assert.equal(natursystem.erGrunntype("NA-T44-E-1"), false);
+    assert.equal(natursystem.erGrunntype("NN-NA-TI-T44-E-1"), false);
   });
   it("T44-1 er grunntype", function() {
-    assert.equal(natursystem.erGrunntype("NA-T44-1"), true);
+    assert.equal(natursystem.erGrunntype("NN-NA-TI-T44-1"), true);
   });
   it("T44-1 erHøyereKartleggingsnivå: nei", function() {
-    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-1"), false);
+    assert.equal(natursystem.erHøyereKartleggingsnivå("NN-NA-TI-T44-1"), false);
   });
   it("T44-A-1 erHøyereKartleggingsnivå: nei", function() {
-    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-A-1"), false);
+    assert.equal(
+      natursystem.erHøyereKartleggingsnivå("NN-NA-TI-T44-A-1"),
+      false
+    );
   });
   it("T44-B-1 erHøyereKartleggingsnivå: ja", function() {
-    assert.equal(natursystem.erHøyereKartleggingsnivå("NA-T44-B-1"), true);
+    assert.equal(
+      natursystem.erHøyereKartleggingsnivå("NN-NA-TI-T44-B-1"),
+      true
+    );
   });
 });
